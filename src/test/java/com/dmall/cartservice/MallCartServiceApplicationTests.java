e9efe18d1c02ce8e7ceec77e933268afa48a614c
ImHyukJin <ohj9575@gmail.com>
2025-03-12 17:51:06 +0900

    chore: gradle 수정

---
+++ src/test/java/com/dmall/cartservice/MallCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmall.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
