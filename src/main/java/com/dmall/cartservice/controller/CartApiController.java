7c8413dd3261313e86cadecd4bf3c37598629293
ImHyukJin <ohj9575@gmail.com>
2025-04-02 22:56:13 +0900

    feat: api 서버 신설

---
+++ src/main/java/com/dmall/cartservice/controller/CartApiController.java
@@ -0,0 +1,43 @@
+package com.dmall.cartservice.controller;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dmall.cartservice.repository.CartRepository;
+
+public class CartApiController {
+	
+	@Autowired
+	private CartRepository cartRepository ;
+	
+	
+	@Value("${server.env}")
+	private String env;
+	@Value("${server.port}")
+	private String serverPort;
+	@Value("${server.address}")
+	private String serverAddress;
+	@Value("${serverName}")
+	private String serverName;
+	
+	//서버가 잘 돌아가나
+	@GetMapping("/hc")
+	public ResponseEntity<?> healthCheck() {
+		Map<String, String> responseData = new TreeMap<>();
+		responseData.put("env", env);
+		responseData.put("serverPort", serverPort);
+		responseData.put("serverAddress", serverAddress);
+		responseData.put("serverName", serverName);
+		return ResponseEntity.ok(responseData);
+	}
+	
+	@GetMapping("/env")
+	public ResponseEntity<?> getEnv() {
+		return ResponseEntity.ok(env);
+	}
+}
