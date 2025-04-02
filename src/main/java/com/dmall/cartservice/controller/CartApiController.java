1a49af7b8067d904c197089acc8a8d80a021852b
ImHyukJin <ohj9575@gmail.com>
2025-04-02 23:35:59 +0900

    feat: RestController 추가

--- src/main/java/com/dmall/cartservice/controller/CartApiController.java
+++ src/main/java/com/dmall/cartservice/controller/CartApiController.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dmall.cartservice.repository.CartRepository;
 
+@RestController
+@RequestMapping("/api/cart")
 public class CartApiController {
 	
 	@Autowired
