60f1f4ae3d5c6aa6e9f703c388103a67c5a1afbc
ImHyukJin <ohj9575@gmail.com>
2025-04-03 10:36:50 +0900

    ff

--- src/main/java/com/dmall/cartservice/controller/CartApiController.java
+++ src/main/java/com/dmall/cartservice/controller/CartApiController.java
@@ -1,5 +1,6 @@
 package com.dmall.cartservice.controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dmall.cartservice.model.Cart;
 import com.dmall.cartservice.repository.CartRepository;
 
 @RestController
@@ -44,4 +48,17 @@ public class CartApiController {
 	public ResponseEntity<?> getEnv() {
 		return ResponseEntity.ok(env);
 	}
+	
+	 // 장바구니 추가
+    @PostMapping
+    public String addToCart(@RequestBody Cart cart) {
+        Cart savedCart = cartRepository.save(cart); // JPA 저장
+        return "장바구니에 추가됨! ID: " + savedCart.getCartId();
+    }
+
+    // 장바구니 목록 조회
+    @GetMapping
+    public List<Cart> getAllCarts() {
+        return cartRepository.findAll(); // JPA 조회
+    }
 }
--- src/main/java/com/dmall/cartservice/controller/HealthCheckController.java
+++ src/main/java/com/dmall/cartservice/controller/HealthCheckController.java
@@ -1,18 +1,28 @@
 package com.dmall.cartservice.controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dmall.cartservice.model.Cart;
+import com.dmall.cartservice.repository.CartRepository;
 
 @RestController
 @RequestMapping("/cart")
 public class HealthCheckController {
 
+	@Autowired
+	private CartRepository cartRepository ;
+	
 	@Value("${server.env}")
 	private String env;
 	@Value("${server.port}")
@@ -37,4 +47,16 @@ public class HealthCheckController {
 	public ResponseEntity<?> getEnv() {
 		return ResponseEntity.ok(env);
 	}
+	 // 장바구니 추가
+    @PostMapping
+    public String addToCart(@RequestBody Cart cart) {
+        Cart savedCart = cartRepository.save(cart); // JPA 저장
+        return "장바구니에 추가됨! ID: " + savedCart.getCartId();
+    }
+
+    // 장바구니 목록 조회
+    @GetMapping
+    public List<Cart> getAllCarts() {
+        return cartRepository.findAll(); // JPA 조회
+    }
 }
